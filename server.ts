e366356d4db4259f2f251b361b262ce2eb6a17f8
kepler-b <abhishek.codecraft@gmail.com>
2024-07-11 13:14:42 +0530

    adding env config

--- server.ts
+++ server.ts
@@ -1,9 +1,9 @@
 import { Server, Socket } from "socket.io";
-import  {createServer} from "http";
+import { createServer } from "http";
 import { randomBytes } from "crypto";
 import cors from "cors";
 import express, { Request, Response } from "express";
-const { bgRed, red, yellow, bgGreenBright, greenBright } = require('colorette');
+import { bgRed, red, yellow, bgGreenBright, greenBright } from 'colorette';
 
 const app = express();
 const httpServer = createServer(app);
@@ -15,16 +15,16 @@ const io = new Server(httpServer, {
 });
 
 type TSocketAndUser = {
-  socket: Socket,
-  username: string,
+  socket: Socket;
+  username: string;
 };
 
 type TRoomAndUser = {
-  username: string,
-  uid: string
-}
+  username: string;
+  uid: string;
+};
 
-type OnJoinEventData = { uid: string, username: string };
+type OnJoinEventData = { uid: string; username: string };
 
 const userToSocketMapping = new Map<string, TSocketAndUser>();
 const adminAndRoomID = new Map<string, TRoomAndUser>();
@@ -33,33 +33,31 @@ function createRoomID() {
   return randomBytes(16).toString("hex");
 }
 
-
 function printError(...msg: any[]) {
-  console.log(bgRed("Error:"), red(msg.join(' ')));
+  console.log(bgRed("Error:"), red(msg.join(" ")));
 }
 
 function printInfo(...msg: any[]) {
-  console.log(bgRed("Info:"), yellow(msg.join(' ')));
+  console.log(bgRed("Info:"), yellow(msg.join(" ")));
 }
 
 function printSuccess(...msg: any[]) {
-  console.log(bgGreenBright("Success:"), greenBright(msg.join(' ')));
+  console.log(bgGreenBright("Success:"), greenBright(msg.join(" ")));
 }
-
 
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 app.get("/ip-check", (req, res) => {
-  res.send({ ipAddress: req.ip, ipAddresses: req.ips })
-})
+  res.send({ ipAddress: req.ip, ipAddresses: req.ips });
+});
 
 app.post("/create-room", (req: Request, res: Response) => {
   const { uid, username } = req.body;
   const roomId = createRoomID();
   adminAndRoomID.set(roomId, {
     uid,
-    username
+    username,
   });
   res.send({ roomId });
   printSuccess("Room ID Send Success:", JSON.stringify({ uid, username, roomId }));
@@ -74,13 +72,13 @@ io.on("connection", (socket: Socket) => {
     if (adminAndRoomID.has(roomId)) {
       const creatorID = adminAndRoomID.get(roomId)!;
       if (!userToSocketMapping.has(creatorID.uid)) {
-        printError("On-Call-User: Not Found", JSON.stringify({ creatorID, roomId }))
+        printError("On-Call-User: Not Found", JSON.stringify({ creatorID, roomId }));
         return;
       }
 
       const creatorSocket = userToSocketMapping.get(creatorID.uid)!;
       creatorSocket.socket.emit("call-offer", { offer, otherUID: uid });
-      printSuccess("On-Call-user: call-offer emitted", JSON.stringify({ otherUID: uid }))
+      printSuccess("On-Call-user: call-offer emitted", JSON.stringify({ otherUID: uid }));
     } else {
       socket.emit("error-calling", { message: "Can't Find User or something went wrong." });
     }
@@ -95,7 +93,7 @@ io.on("connection", (socket: Socket) => {
 
   socket.on("creator-ice-candidate", ({ otherUID, icecandidate }) => {
     if (!userToSocketMapping.has(otherUID)) {
-      printError("On-creator-ice-candidate: User Socket Not Found", JSON.stringify({ otherUID }))
+      printError("On-creator-ice-candidate: User Socket Not Found", JSON.stringify({ otherUID }));
       return;
     }
 
@@ -103,24 +101,26 @@ io.on("connection", (socket: Socket) => {
     socket.emit("recv-ice-candidate", { candidate: icecandidate });
     printSuccess("On-creator-ice-candidate: Creator Candidate Send", JSON.stringify({ otherUID, username }));
   });
+
   socket.on("joined-ice-candidate", ({ roomId, icecandidate }): void => {
     if (!adminAndRoomID.has(roomId)) {
-      printError("On-join-ice-candidate: can't find admin id with roomid or admin doesn't exists");
-    return;
+      printError("On-join-ice-candidate: can't find admin id with roomid or admin doesn't exist");
+      return;
     }
-    
+
     const creatorId = adminAndRoomID.get(roomId)!;
     if (!userToSocketMapping.has(creatorId.uid)) {
       printError("On-join-ice-candidate: can't find admin socket with uid");
       return;
     }
     const { socket } = userToSocketMapping.get(creatorId.uid)!;
-    
+
     socket.emit("recv-ice-candidate", { candidate: icecandidate });
     printSuccess("On-join-ice-candidate: Joined Candidate Send", JSON.stringify({ roomId, creatorId }));
   });
 });
 
-httpServer.listen(2000, () => {
-  console.log("Server is listening on port 2000");
+const PORT = process.env.PORT || 2000;
+httpServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
